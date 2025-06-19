0b0bf05a939949f77f44e4655908927599883e2f
Wanwei Jiang <wanwei.jiang@amlogic.com>
2025-06-19 16:38:26 +0800

    ddr bandwidth: support lpddr5 [1/2]
    PD#SWPL-191305
    
    Problem:
    the frequency and bandwidth of LPDDR5 are inaccurate
    
    Solution:
    the frequency calculation methods for LPDDR5 and DDR3/2 are different
    
    Verify:
    local
    
    Change-Id: I994d9f77dd9e30c799f6071060a1f1bc704e61e3
    Signed-off-by: Wanwei Jiang <wanwei.jiang@amlogic.com>

--- drivers/memory_debug/ddr_tool/ddr_bandwidth.h
+++ drivers/memory_debug/ddr_tool/ddr_bandwidth.h
@@ -109,6 +109,31 @@
 #define DDR_PRIORITY_DEBUG		BIT(31)
 #define DDR_PRIORITY_POWER		BIT(30)
 
+enum ddr_type {
+	DDR3 = 0,
+	DDR4,
+	LPDDR4,
+	LPDDR3,
+	LPDDR2,
+	LPDDR4X,
+	LPDDR5,
+};
+
+enum outstanding_type {
+	OUTSTANDING_SET = 1,
+	OUTSTANDING_GET,
+};
+
+enum property_type {
+	WBUF_EMPTY = 1,
+	WBUF_H,
+	WBUF_M,
+	WBUF_L,
+	RBUF_H,
+	RBUF_M,
+	RBUF_L,
+};
+
 struct ddr_bandwidth;
 
 struct ddr_grant {
@@ -187,6 +212,7 @@ struct ddr_bandwidth {
 	unsigned int channels;
 	unsigned int dmc_number;
 	unsigned int usage_stat[10];
+	enum ddr_type ddr_type;
 	unsigned long ddr_freq;
 	unsigned long dmc_freq;
 	spinlock_t lock;		/* lock for usage statistics */
