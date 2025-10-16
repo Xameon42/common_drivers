db8ea07992344fc9f665cbb9ed2cb8083f83d656
Long Yu <long.yu@amlogic.com>
2025-10-16 11:05:53 +0800

    sdio: need to compatible with w1. [1/1]
    PD#SWPL-235426
    
    Problem:
    Not compatible with w1.
    
    Solution:
    Need to be compatible with w1..
    
    Verify:
    S7D
    
    Change-Id: I4a7a1ece3be10694e7edc304a27312b714e244d1
    Signed-off-by: Long Yu <long.yu@amlogic.com>

--- drivers/mmc/host/meson-gx-mmc.c
+++ drivers/mmc/host/meson-gx-mmc.c
@@ -607,8 +607,10 @@ static int no_pxp_clk_set(struct meson_host *host, struct mmc_ios *ios,
 		}
 		/* Switch sdio source clock to DIV2 to provide bandwidth */
 		if (sdio_host && sdio_host->card &&
-			host->vendor_id &&
-			sdio_host->card->cis.vendor == host->vendor_id)
+			((host->vendor_id &&
+			sdio_host->card->cis.vendor == host->vendor_id) ||
+			/* Need to be compatible with w1. */
+			(sdio_host->card->cis.vendor == 0x8888)))
 			src_clk = host->clk[2];
 		else
 			src_clk = host->clk[1];
